bc831c124dbef7e4f1def1cb8a9630d15d956dc9
ngnm1009 <ngnm1009@gmail.com>
2021-08-24 10:11:09 +0700

    fix dev tool

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,15 @@
 import axiosService from "../service/axiosService";
 
-const key = '80a9ad015d8ce64e904fc91dc142562c';
+const key = "80a9ad015d8ce64e904fc91dc142562c";
 export const getWeather = (name) => {
-
-    return axiosService.get(`https://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&APPID=${key}`)
-}
+  return axiosService.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&APPID=${key}`
+  );
+};
 
 export const getOneCall = (toado) => {
-    const {lat,lon} = toado
-    return axiosService.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&APPID=${key}`)
-}
+  const { lat, lon } = toado;
+  return axiosService.get(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&APPID=${key}`
+  );
+};
--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import {createStore, compose, applyMiddleware} from "redux";
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from "./weatherReducer";
 import rootSaga from "./rootSaga";
-const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composeEnhancers = (process.env.NODE_ENV === 'development' && typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const sagaMiddleware = createSagaMiddleware()
 const configureStore = () => {
 
