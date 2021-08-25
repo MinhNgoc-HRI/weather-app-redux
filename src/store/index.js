125236f5d852e299d0c3016638f944b73781aaa2
ngnm1009 <ngnm1009@gmail.com>
2021-08-25 14:05:23 +0700

    fix toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
-import {createStore, compose, applyMiddleware} from "redux";
-import createSagaMiddleware from 'redux-saga';
+import { createStore, compose, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 import rootReducer from "./weatherReducer";
 import rootSaga from "./rootSaga";
-const composeEnhancers = (process.env.NODE_ENV === 'development' && typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-const sagaMiddleware = createSagaMiddleware()
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+const sagaMiddleware = createSagaMiddleware();
 const configureStore = () => {
-
-    const middleware = [sagaMiddleware];
-    const enhancers = [applyMiddleware(...middleware)]
-    const store = createStore(rootReducer,composeEnhancers(...enhancers));
-    sagaMiddleware.run(rootSaga)
-    return store;
+  const middleware = [sagaMiddleware];
+  const enhancers = [applyMiddleware(...middleware)];
+  const store = createStore(rootReducer, composeEnhancers(...enhancers));
+  sagaMiddleware.run(rootSaga);
+  return store;
 };
 export default configureStore;
